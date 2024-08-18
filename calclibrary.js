195b9e471f8cbfafe5cbf4d8bbd0889cb5cc954a
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-08-18 13:30:20 +0300

    Set up calclibrary.js file

---
+++ calclibrary.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function sum(a, b) {
+  if (arguments.length < 2) {
+    throw new Error("parameter missing");
+  }
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('only numbers allowed');
+  }
+  return a + b;
+};
+
+module.exports = { sum };
